1ea29227503182df2b798eac115946bb78ebf51a
Sufiyyan Panezai <2572293p@student.gla.ac.uk>
2021-02-13 13:35:21 +0800

    Seventh commit

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rango import views 
+from djnago.conf import settings 
+from django.conf.urls.static import static
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('admin/', admin.site.urls),
-]
+path('', views.index, name='index'),
+path('about/', views.about, name='about'),
+path('category/<slug:category_name_slug>/',
+views.show_category, name='show_category'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
